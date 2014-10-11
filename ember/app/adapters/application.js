eaf201144414adc29900623241fb5a8f1b76f2c1
Huafu Gandon <huafu.gandon@gmail.com>
2014-10-11 13:08:16 +0700

    Improved the adapter and uses default rest serializer

--- ember/app/adapters/application.js
+++ ember/app/adapters/application.js
@@ -5,11 +5,84 @@ import Ember from 'ember';
 var RSVP = Ember.RSVP;
 
 
+function newPayload(store, type, record) {
+  var res = {}, extracted;
+  if (arguments.length > 0) {
+    extracted = [];
+    payloadInject(store, res, type, record, extracted);
+  }
+  return res;
+}
+
+function payloadInject(store, payload, type, record, _extracted) {
+  var index, records, toExtract,
+    typeKey = (type.typeKey ? type.typeKey : type).pluralize();
+  if (!payload[typeKey]) {
+    payload[typeKey] = [];
+  }
+  if (record) {
+    records = Ember.typeOf(record) === 'array' ? record : [record];
+    index = {};
+    toExtract = [];
+    payload[typeKey].forEach(function (record) {
+      index['' + record.id] = 0;
+    });
+    records.forEach(function (record) {
+      var id = '' + record.id;
+      if (!record.id) {
+        throw new ReferenceError('Got a record without id for ' + typeKey);
+      }
+      if (!(id in index)) {
+        index[id] = 0;
+        payload[typeKey].push(record);
+        toExtract.push(record);
+      }
+    });
+    toExtract.forEach(function (record) {
+      payloadExtractEmbedded(store, payload, type, record, _extracted);
+    });
+  }
+  return payload;
+}
+
+function payloadExtractEmbedded(store, payload, type, record, _extracted) {
+  var extracted = _extracted ? _extracted : [];
+  if (extracted.indexOf(record) < 0) {
+    extracted.push(record);
+    type.eachRelationship(function (key, rel) {
+      var data;
+      if ((data = record[key])) {
+        if (rel.kind === 'belongsTo') {
+          if (Ember.typeOf(record[key]) === 'object') {
+            delete record[key];
+            payloadInject(store, payload, rel.type, data, extracted);
+            record[key] = data.id;
+          }
+        }
+        else if (rel.kind === 'hasMany') {
+          record[key] = data.map(function (item) {
+            if (Ember.typeOf(item) === 'object') {
+              payloadInject(store, payload, rel.type, item, extracted);
+              return item.id;
+            }
+            return item;
+          });
+        }
+        else {
+          throw new ReferenceError('Unknown relationship kind ' + rel.kind);
+        }
+      }
+    });
+  }
+  return payload;
+}
+
 export default DS.RESTAdapter.extend({
-  namespace: 'api/v1',
-  useCSRF:   true,
-  log:       true,
-  debug:     true,
+  namespace:            'api/v1',
+  useCSRF:              true,
+  log:                  true,
+  debug:                true,
+  coalesceFindRequests: true,
 
   CSRFToken:       '',
   listeningModels: {},
@@ -47,8 +120,7 @@ export default DS.RESTAdapter.extend({
   },
 
   pathForType: function (type) {
-    var camelized = Ember.String.camelize(type);
-    return Ember.String.singularize(camelized);
+    return Ember.String.camelize(type).pluralize();
   },
 
   isErrorObject: function (data) {
@@ -91,11 +163,47 @@ export default DS.RESTAdapter.extend({
 
 
   createRecord: function (store, type, record) {
-    var data = {};
     var serializer = store.serializerFor(type.typeKey);
-    serializer.serializeIntoHash(data, type, record, { includeId: true });
-    return this.ajax(this.buildURL(type.typeKey, null, record), "POST", data);
+    var data = serializer.serialize(record, { includeId: true });
+    return this.ajax(this.buildURL(type.typeKey, null, record), "POST", data).then(function (payload) {
+      return newPayload(store, type, payload);
+    });
   },
+
+
+  find: function (store, type/*, id, record*/) {
+    return this._super.apply(arguments).then(function (payload) {
+      return newPayload(store, type, payload);
+    });
+  },
+
+
+  findAll: function (store, type/*, sinceToken*/) {
+    return this._super.apply(this, arguments).then(function (payload) {
+      return newPayload(store, type, payload);
+    });
+  },
+
+  findBelongsTo: function (/*store, record, url*/) {
+    // TODO: check what is returning Sails in that case
+    return this._super.apply(this, arguments);
+  },
+
+  findHasMany: function (/*store, record, url*/) {
+    // TODO: check what is returning Sails in that case
+    return this._super.apply(this, arguments);
+  },
+
+  findMany: function (store, type, ids/*, records*/) {
+    return this.findQuery(store, type, {where: {id: ids}});
+  },
+
+  findQuery: function (store, type, query) {
+    return this.ajax(this.buildURL(type.typeKey), 'GET', query).then(function (payload) {
+      return newPayload(store, type, payload);
+    });
+  },
+
 
   checkCSRF: function (data) {
     if (!this.useCSRF) {
@@ -115,41 +223,38 @@ export default DS.RESTAdapter.extend({
       return;
     }
     var store = this.container.lookup('store:main');
-    var socketModel = model;
+    var type = store.modelFor(model);
+    var eventName = Ember.String.camelize(model).toLowerCase();
 
     function pushMessage(message) {
-      var type = store.modelFor(socketModel);
-      var serializer = store.serializerFor(type.typeKey);
-      // Messages from 'created' don't seem to be wrapped correctly,
-      // however messages from 'updated' are, so need to double check here.
-      if (!(model in message.data)) {
-        var obj = {};
-        obj[model] = message.data;
-        message.data = obj;
+      var payload, record;
+      if (message.verb === 'created') {
+        record = message.data;
       }
-      var record = serializer.extractSingle(store, type, message.data);
-      // If the id is not present in the record, get it from the message
-      if (!record[socketModel].id) {
-        record[socketModel].id = message.id;
+      else {
+        record = message.data[type.typeKey];
       }
-      store.push(socketModel, record[socketModel]);
+      if (!record.id && message.id) {
+        record.id = message.id;
+      }
+      payload = newPayload(store, type, record);
+      store.push(model, payload);
     }
 
     function destroy(message) {
-      var type = store.modelFor(socketModel);
       var record = store.getById(type, message.id);
-
       if (record && typeof record.get('dirtyType') === 'undefined') {
         record.unloadRecord();
       }
     }
 
-    var eventName = Ember.String.camelize(model).toLowerCase();
+
     io.socket.on(eventName, function (message) {
       this._debug('[socket][' + eventName + '] new message', message);
+
       if (message.verb === 'created') {
         // Run later to prevent creating duplicate records when calling store.createRecord
-        Ember.run.later(null, pushMessage, message, 50);
+        Ember.run.next(null, pushMessage, message);
       }
       if (message.verb === 'updated') {
         pushMessage(message);
@@ -164,6 +269,7 @@ export default DS.RESTAdapter.extend({
     this.listeningModels[model] = 0;
     this._debug('[socket] listening to `' + eventName + '` for `' + model + '`');
   },
+
 
   _log: function () {
     if (this.log) {
