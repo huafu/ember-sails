267ebcb6118b2cf09d4bf5d15e3b14768f8ba14c
Huafu Gandon <huafu.gandon@gmail.com>
2014-10-20 23:15:25 +0700

    More work on google map

--- ember/app/initializers/google-map.js
+++ ember/app/initializers/google-map.js
@@ -1,16 +1,12 @@
-import GoogleMapComponent from '../components/google-map';
+import MarkerController from '../components/google-map/controllers/marker';
+import MarkerView from '../components/google-map/views/marker';
 
 export var initialize = function (container, application) {
-  application.register(
-    'controller:google-map/marker',
-    GoogleMapComponent.GoogleMapMarkerController,
-    {singleton: false}
-  );
-  application.register(
-    'controller:google-map/markers',
-    GoogleMapComponent.GoogleMapMarkersController,
-    {singleton: false}
-  );
+
+  application.register('controller:google-map/marker', MarkerController, {singleton: false});
+
+  application.register('view:google-map/marker', MarkerView, {singleton: false});
+
 };
 
 export default {
