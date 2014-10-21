d38f1b02f5ce2dfe3fd56a9da058669aaec8801c
Huafu Gandon <huafu.gandon@gmail.com>
2014-10-21 11:03:06 +0700

    Adding a delay to live-reload

--- ember/app/extensions.js
+++ ember/app/extensions.js
@@ -1,29 +1,40 @@
 import Ember from 'ember';
 
 /* Modernizr is doing this
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function (context) {
-    if (typeof this !== "function") {
-      // closest thing possible to the ECMAScript 5
-      // internal IsCallable function
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-    }
-    var args = Array.prototype.slice.call(arguments, 1),
-      method = this,
-      Noop = function () {
-      },
-      res = function () {
-        return method.apply(
-            this instanceof Noop && context ? this : context,
-          args.concat(Array.prototype.slice.call(arguments))
-        );
+ if (!Function.prototype.bind) {
+ Function.prototype.bind = function (context) {
+ if (typeof this !== "function") {
+ // closest thing possible to the ECMAScript 5
+ // internal IsCallable function
+ throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+ }
+ var args = Array.prototype.slice.call(arguments, 1),
+ method = this,
+ Noop = function () {
+ },
+ res = function () {
+ return method.apply(
+ this instanceof Noop && context ? this : context,
+ args.concat(Array.prototype.slice.call(arguments))
+ );
+ };
+ Noop.prototype = this.prototype;
+ res.prototype = new Noop();
+ return res;
+ };
+ }
+ */
+
+Ember.runInDebug(function overrideLiveReload() {
+  if (window.LiveReload) {
+    Ember.debug('[live-reload] overriding live-reload delay...');
+    window.LiveReload.reloader.reloadPage = (function (reloadPage) {
+      return function () {
+        setTimeout(reloadPage.bind(this), 1000);
       };
-    Noop.prototype = this.prototype;
-    res.prototype = new Noop();
-    return res;
-  };
-}
-*/
+    })(window.LiveReload.reloader.reloadPage);
+  }
+});
 
 Ember.View.reopen({
   setupFoundation: function () {
